8f3f166429a6125b22dd30f9718daac8a184f426
sesana.federico <sesana.federico@TAVE-PC05.OSDBSESTO.local>
2021-04-15 14:08:43 +0200

    Serialization thingything

--- Serializzazione/src/serializzazione/Serializzazione.java
+++ Serializzazione/src/serializzazione/Serializzazione.java
@@ -5,6 +5,13 @@
  */
 package serializzazione;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  *
  * @author sesana.federico
@@ -14,8 +21,30 @@ public class Serializzazione {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
         Studente studente = new Studente("Mimmo", "Carrozza", "69420");
+        
+        FileOutputStream uwu = new FileOutputStream("testpuntoser.ser");
+        ObjectOutputStream sus = new ObjectOutputStream(uwu);
+        sus.writeObject(69);
+        sus.writeObject("°U°");
+        sus.writeObject((float)4.20);
+        sus.writeObject(false);
+        
+        sus.flush();
+        uwu.flush();
+        uwu.close();
+        sus.close();
+        
+        FileInputStream owo = new FileInputStream("testpuntoser.ser");
+        ObjectInputStream sos = new ObjectInputStream(owo);
+        
+        int valoreuno = (int) sos.readObject();
+        System.out.println(valoreuno);
+        String valoredue = (String) sos.readObject();
+        System.out.println(valoredue);
+        float valoretre = (float) sos.readObject();
+        System.out.println(valoretre);
     }
     
 }
