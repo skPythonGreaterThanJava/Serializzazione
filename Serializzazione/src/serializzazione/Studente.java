1d6b5260ce3005513ca50eebe15db7e97584ab7f
sesana.federico <sesana.federico@TAVE-PC05.OSDBSESTO.local>
2021-04-15 13:01:18 +0200

    Base stuff

---
+++ Serializzazione/src/serializzazione/Studente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializzazione;
+
+/**
+ *
+ * @author sesana.federico
+ */
+public class Studente {
+    String nome, cognome, numero;
+
+    public Studente(String nome, String cognome, String numero) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.numero = numero;
+    }
+    
+    
+}
